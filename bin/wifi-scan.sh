53f90cb2584b102e5ba84f815d7d1581f8d83b45
Guy Cole <guycole@gmail.com>
2022-02-25 11:43:32 -0800

    stub wifi scan

---
+++ bin/wifi-scan.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Title:wifi-scan.sh
+#
+# Description: scan for wireless access points
+#
+# Development Environment: Debian 10 (buster)/raspian
+#
+# Author: Guy Cole (guycole at gmail dot com)
+#
+PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
+#
+echo 'go away'
