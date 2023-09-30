1ebb2382858673a801aafde145e275e1c12255e9
Guy Cole <guycole@gmail.com>
2023-09-30 12:25:40 -0700

    add file transfer

---
+++ bin/rpi-xfer.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Title: rpi-xfer.sh
+# Description: move local files to s3
+# Development Environment: Debian 10 (buster)/raspian
+# Author: Guy Cole (guycole at gmail dot com)
+#
+PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
+#
+echo "start transfer"
+cd /home/gsc/github/mellow-heeler
+aws s3 mv aws_export s3://mellow-heeler.braingang.net/vallejo1 --profile=heeler-rpi --recursive
+echo "end transfer"
