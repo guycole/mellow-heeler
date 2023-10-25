395099cb42bc994eff3b0357bd5cfb5af7d1dbc6
Guy Cole <guycole@gmail.com>
2023-10-25 01:46:58 +0000

    anderson collection

---
+++ bin/rpi-xfer-anderson.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+# Title: rpi-xfer-anderson.sh
+# Description: move local files to s3
+# Development Environment: Debian 10 (buster)/raspian
+# Author: Guy Cole (guycole at gmail dot com)
+#
+PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
+#
+echo "start transfer"
+cd /home/gsc/github/mellow-heeler
+aws s3 mv aws_export s3://mellow-heeler.braingang.net/anderson1 --profile=heeler-rpi --recursive
+echo "end transfer"
