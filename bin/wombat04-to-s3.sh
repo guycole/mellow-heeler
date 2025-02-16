88b94fa92aabb03a61153bda2205e17e439faf6a
Guy Cole <guycole@gmail.com>
2025-02-16 11:08:46 -0800

    add wombat04

---
+++ bin/wombat04-to-s3.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#
+# Title: wombat04-to-s3.sh
+# Description: move heeler files local file system to s3
+# Development Environment: Ubuntu 22.04.05 LTS
+# Author: Guy Cole (guycole at gmail dot com)
+#
+PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
+#
+DEST_BUCKET=s3://mellow-heeler-uw2-k2718.braingang.net/fresh/
+#
+echo "start move"
+cd /var/mellow/heeler/fresh; gzip *
+aws s3 mv . $DEST_BUCKET --recursive --profile=wombat04
+echo "end move"
