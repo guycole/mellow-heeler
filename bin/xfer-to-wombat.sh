da730b982d6a48c05eb9da6c1eb6320876962fee
Guy Cole <guycole@gmail.com>
2025-02-20 05:50:56 +0000

    rpi3a wombat update

--- bin/xfer-to-wombat.sh
+++ bin/xfer-to-wombat.sh
@@ -1,14 +1,14 @@
 #!/bin/bash
 #
-# Title: xfer-wombat.sh
+# Title: xfer-to-wombat.sh
 # Description: move local files to wombat, needs ssh authorized_keys for $DEST
 # Development Environment: Debian 10 (buster)/raspian
 # Author: Guy Cole (guycole at gmail dot com)
 #
 PATH=/bin:/usr/bin:/etc:/usr/local/bin:~/.local/bin; export PATH
 #
-DEST="gsc@rpi5a:/mnt/pp1/gsc/mellow"
-SRC="/var/mellow"
+DEST="gsc@rpi5a:/mnt/pp1/gsc/mellow/heeler"
+SRC="/var/mellow/heeler/fresh"
 #
 echo "start transfer"
 cd $SRC
