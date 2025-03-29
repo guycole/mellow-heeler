213b5a2a3e70f7dbdd7a6bd3a14f3cce733757cd
Guy Cole <guycole@gmail.com>
2025-03-29 02:43:29 +0000

    latest box scores

--- bin/export-to-s3.sh
+++ bin/export-to-s3.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 #
-# Title: archiver.sh
-# Description: move loaded files to archive
+# Title: export-to-s3.sh
+# Description: move exported files to s3 archive
 # Development Environment: macOS Monterey 12.6.9
 # Author: Guy Cole (guycole at gmail dot com)
 #
@@ -10,12 +10,12 @@ PATH=/bin:/usr/bin:/etc:/usr/local/bin; export PATH
 TODAY=$(date '+%Y-%m-%d')
 FILE_NAME="heeler-${TODAY}.tgz"
 #
-DEST_BUCKET=s3://mellow-heeler-uw2-k2718.braingang.net/archive/
+DEST_BUCKET=s3://mellow-heeler-uw2-k2718.braingang.net/exporter/
 #
-SOURCE_DIR="archive"
+SOURCE_DIR="export"
 WORK_DIR="/var/mellow/heeler"
 #
-echo "start archive"
+echo "start export"
 #
 cd ${WORK_DIR}
 tar -cvzf ${FILE_NAME} ${SOURCE_DIR}
@@ -27,5 +27,5 @@ echo "cleanup"
 rm -rf ${SOURCE_DIR}
 mkdir ${SOURCE_DIR}
 #
-echo "end archive"
+echo "end export"
 #
